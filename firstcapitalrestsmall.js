174551a788dfa7d4e832c31e85347297e8b7a50c
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-11-28 19:55:53 +0530

    Update firstcapitalrestsmall.js

--- firstcapitalrestsmall.js
+++ firstcapitalrestsmall.js
@@ -5,3 +5,11 @@ first=first.toUpperCase();
 second=second.toLowerCase();
 var third=first+second;
 alert("welcome "+third);
+
+
+//MY CODE
+var name= prompt("Enter your name:");
+var cap=name.slice(0,1);
+var remain=name.slice(1,8);
+var upper=cap.toUpperCase();
+alert("Hello "+upper+remain );
