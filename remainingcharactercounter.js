2c18b96c94e37a54f2ce775528bcb4bd88d1d185
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-11-28 18:14:11 +0530

    Update remainingcharactercounter.js

--- remainingcharactercounter.js
+++ remainingcharactercounter.js
@@ -1,3 +1,13 @@
-var tweet=prompt("write your text");
-var tweetcount=tweet.length;
-alert("you have written " + tweetcount +" characters " + "reamaing charater is " + (140-tweetcount)  );
+//tweet chaeacter count
+// var tweet=prompt("write your text");
+// var tweetcount=tweet.length;
+// alert("you have written " + tweetcount +" characters " + "reamaing charater is " + (140-tweetcount)  );
+
+OR
+
+//tweet chaeacter count
+var tweet=prompt("enter your tweet :");
+var leng=tweet.length;
+var totallen=180;
+var remain=totallen-leng;
+alert("you have written " + leng + " characters" + " remaining characters is " + remain);
