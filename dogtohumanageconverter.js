3d1b6cf3c0d3a6b6854317a9799bc7bd05fa55b4
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-11-28 20:56:05 +0530

    Update dogtohumanageconverter.js

--- dogtohumanageconverter.js
+++ dogtohumanageconverter.js
@@ -1,3 +1,3 @@
 var dogage=prompt("enter the age of the dog to calculate human age");
 var humanage=(dogage-2)*4+21;
-alert("Human age is "+humanage);
+alert("Your dog is  "+humanage + " old equivalent to human age");
