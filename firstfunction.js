9e9a070104c29b4bd468608b9b061b3e7695cead
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-11-30 21:21:48 +0530

    Update firstfunction.js

--- firstfunction.js
+++ firstfunction.js
@@ -1,6 +1,19 @@
+//without using point values
 function myfirst(age)
     {
         var myage = age * 100;
+        console.log("my age is " +myage+ " lol");
+    }
+myfirst(5);
+
+
+
+
+
+////without using point values
+function myfirst(age)
+    {
+        var myage = Math.floor(age * 100.25);
         console.log("my age is " +myage+ " lol");
     }
 myfirst(5);
