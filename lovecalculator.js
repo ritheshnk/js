767da6a749ed1860e0180f6c4e6f504bf4ebd35a
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-12-01 20:20:11 +0530

    Create lovecalculator.js

---
+++ lovecalculator.js
@@ -0,0 +1,6 @@
+var c1= prompt("Enter your name:");
+var c2= prompt("Enter name of your crush:");
+var n = Math.random();
+n=n*100;
+n=Math.floor(n);
+alert("the love percentage of " + c1 + " and "+ c2 + " is "+ n);
