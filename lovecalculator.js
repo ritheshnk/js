d4ed67ade2e680552fe7487ec70dffa197e7dacc
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-12-01 20:41:10 +0530

    Update lovecalculator.js

--- lovecalculator.js
+++ lovecalculator.js
@@ -3,4 +3,12 @@ var c2= prompt("Enter name of your crush:");
 var n = Math.random();
 n=n*100;
 n=Math.floor(n);
-alert("the love percentage of " + c1 + " and "+ c2 + " is "+ n +"%");
+
+if( n>70)
+{
+    alert("the love percentage of " + c1 + " and "+ c2 + " is "+ n + "% " +"Your Love bound is unbreakable");
+}else if( n>50){
+    alert("the love percentage of " + c1 + " and "+ c2 + " is "+ n + "%");
+}else{
+    alert("the love percentage of " + c1 + " and "+ c2 + " is "+ n + "% hope so ur relationship works out ");
+}
