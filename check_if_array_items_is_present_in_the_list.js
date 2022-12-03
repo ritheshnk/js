8df7684501fe0c950ca6e3b82ab1bb48138071fd
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-12-03 23:26:53 +0530

    Create check_if_array_items_is_present_in_the_list.js

---
+++ check_if_array_items_is_present_in_the_list.js
@@ -0,0 +1,8 @@
+var list=["rocky","roman","seth","dean"];
+var mem=prompt("Enter your Name :");
+if(list.includes(mem))
+{
+    alert("WELCOME "+ mem + " to party");
+}else{
+    alert("My i know you   ");
+}
