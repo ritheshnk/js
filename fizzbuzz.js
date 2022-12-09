7682e47af598e85182f2a5d7ab248e5a9f296476
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2022-12-09 23:10:28 +0530

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,22 @@
 var output= [];
-var  count=1;
+// var  count=1;
 function fizzbuzz() {
-    output.push(count);
-    count=count+1;
-
-    if(count%3==0 && count%5==0)
+    for(var count=1;count<=100;count++)
     {
-        output.push("fizzbuzz");
-    }else if(count%3===0)
-    {
-        output.push("FIzz");
-    }else if(count%5===0)
-    {
-        output.push("buzz");
-    }else 
-    {
-        output.push(count);
+        
+        if(count%3==0 && count%5==0)
+        {
+            output.push("fizzbuzz");
+        }else if(count%3===0)
+        {
+            output.push("FIzz");
+        }else if(count%5===0)
+        {
+            output.push("buzz");
+        }else 
+        {
+            output.push(count);
+        }
     }
     console.log(output);
 }
